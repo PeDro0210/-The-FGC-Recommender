e73f19aabb6e6abb8b8cf883b21e6aa019d034c3
PeDro0210 <pedroruben0210@gmail.com>
2024-04-03 23:44:19 -0600

    initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import './styles/app.css';
+
+function App() {
+  return (
+    <div className="App">
+      <text>Cool ass Fighting game webapp</text>{/*Change this obviously */}
+    </div>
+  );
+}
+
+export default App;
