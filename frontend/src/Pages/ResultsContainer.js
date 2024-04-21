51aa478d276723ac0cec4eeb943529d847d3d88c
PeDro0210 <pedroruben0210@gmail.com>
2024-04-21 17:08:03 -0600

    Frontend Structure done

--- frontend/src/Pages/ResultsContainer.js
+++ frontend/src/Pages/ResultsContainer.js
@@ -5,8 +5,21 @@
 export default function ResultsContainer() {
     return (
         <>
-        
+        {/* 
+        For each ImageBox for games, there will be a corresponding ImageBox for the characters below it.
+
+        Example:
+
+        IMAGEBOX
+        CHARACTERBOX CHARACTERBOX2 CHARACTERBOX3 CHARACTERBOX4
+        */}
+
+        <div className="Games">
+        </div>
+        <div className="Characters">
+
+        </div>
         </>
-    )
+    );
 }
 
