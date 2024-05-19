62327d06fed2cc704244e37b97e33c652fc4b6ab
PeDro0210 <pedroruben0210@gmail.com>
2024-05-18 21:55:20 -0600

    Fixing ApiHandler Frontend

--- frontend/src/API/ApiHandler.js
+++ frontend/src/API/ApiHandler.js
@@ -2,27 +2,34 @@
 
 // This is just boilerplate code that I will use to fetch the questions from the back-end
 async function QuestionFetcher() {
-  const response = await fetch('http://localhost:3001/questions'); //the URL is not correct, I will change it later
+  const response = await fetch('http://127.0.0.1:8080/GetQuestions'); //the URL is not correct, I will change it later
   const data = await response.json();
   return data;
 }
 
 async function GamesFetcher(categories) {
-  Query = "http://localhost:3001/GetGames/?categories=" //the URL is not correct, I will change it later
+  Query = "http://127.0.0.1:8080/GetGames/"
   for (let i = 0; i < categories.length; i++) {
-    Query = Query + categories[i] + "&" +"categories="
+    Query = Query + categories[i] +"/"
+    if (i == categories.length - 1) {
+      Query = Query + categories [i] 
+    }
   }
   const response = await fetch(Query);
   const data = await response.json();
   return data;
 }
 
-async function GameFetcher(gameID, Arhetypes) {
-  Query = "http://localhost:3001/GetGame/?game_name=" + gameID + "&archetype="; //the URL is not correct, I will change it later
+async function CharacterFetcher(gameID, Arhetypes) {
+  Query = "http://127.0.0.1:8080/GetGame/"+ gameID + "/"; 
   for (let i = 0; i < Arhetypes.length; i++) {
-    Query = Query + Arhetypes[i] + "&" +"archetype="
+    Query = Query + Arhetypes[i]  +"/"
+    if (i == Arhetypes.length - 1) {
+      Query = Query + Arhetypes[i] 
+    }
   }
   const response = await fetch(Query);
   const data = await response.json();
   return data;
 }
+
