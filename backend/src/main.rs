24dae8b56b37ea615730f5beec27f3620e248ab9
PeDro0210 <pedroruben0210@gmail.com>
2024-06-10 16:11:29 -0600

    Finish DockerFile (Dockerfile ain't on commit)

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,18 +1,25 @@
-use actix_web::{App, HttpServer};
+use actix_web::{App, HttpServer, middleware::Logger, http};
 use actix_cors::Cors;
 mod api;
 
 #[actix_web::main]
-async fn main() -> std::io::Result<()> { //Setting route services
+async fn main() -> std::io::Result<()> {
+    println!("Server is running on http://localhost:8080");
     HttpServer::new(|| {
-        // Fucking CORS
-        let cors = Cors::permissive();
+        let cors = Cors::default()
+            .allowed_origin("http://localhost:3000")
+            .allowed_methods(vec!["GET", "POST"])
+            .allowed_headers(vec![http::header::AUTHORIZATION, http::header::ACCEPT])
+            .max_age(3600);
+
         App::new()
             .wrap(cors)
+            .wrap(Logger::default())
             .service(api::api_handler::question_sender)
             .service(api::api_handler::game_sender)
     })
-    .bind(("127.0.0.1", 8080))?
+    .bind("0.0.0.0:8080")?
     .run()
     .await
 }
+
